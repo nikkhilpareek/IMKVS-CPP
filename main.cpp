afe1d964ffc4d50120e1379eb93407e054046328
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-07-28 13:55:05 +0530

    addd basic crud operations. made minor changes to make the code concise

--- main.cpp
+++ main.cpp
@@ -1,48 +1,55 @@
-#include <string>
 #include <iostream>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 #include <optional>
 
 class KeyValueStore {
 private:
-    // What declaration should go here to store our key-value pairs?
     std::unordered_map<std::string, std::string> data;
 
 public:
-    // We'll add our functions (methods) here later.
-    void set(const std::string &key, const std::string &value){
+    // Set a key-value pair.
+    void set(const std::string& key, const std::string& value) {
         data[key] = value;
-        std::cout<<"OK\n";
     }
 
-    std::optional<std::string> get(const std::string &key) const {
-        auto it = data.find(key);
-        if(it == data.end()){
-            return std::nullopt;
-        }else{
+    // Get a value by its key.
+    std::optional<std::string> get(const std::string& key) const {
+        if (auto it = data.find(key); it != data.end()) {
             return it->second;
         }
+        return std::nullopt;
     }
 
+    // Remove a key-value pair and return true if successful.
+    bool remove(const std::string& key) {
+        return data.erase(key) > 0;
+    }
 };
 
-int main(){
+int main() {
     KeyValueStore kvs;
-    kvs.set("home","ganganagar");
-    kvs.set("college","jaipur");
-    
-    if(auto value = kvs.get("school")){
-        std::cout << "Found value: " << *value << std::endl;
-    }else{
-        std::cout<<"Value not Found"<<std::endl;
+
+    kvs.set("home", "ganganagar");
+    kvs.set("college", "jaipur");
+    std::cout << "Set 'home' and 'college'." << std::endl;
+
+    // Test getting a value that exists
+    if (auto value = kvs.get("college")) {
+        std::cout << "GET 'college': Found value -> " << *value << std::endl;
     }
 
-    if(auto value = kvs.get("college")){
-        std::cout << "Found value: " << *value << std::endl;
-    }else{
-        std::cout<<"Value not Found"<<std::endl;
+    // Test removing a value
+    if (kvs.remove("college")) {
+        std::cout << "REMOVE 'college': Success." << std::endl;
     }
-    
 
+    // Test getting a value that has been removed
+    if (auto value = kvs.get("college")) {
+        std::cout << "GET 'college': Found value -> " << *value << std::endl;
+    } else {
+        std::cout << "GET 'college': Value not found." << std::endl;
+    }
+
+    return 0;
 }
